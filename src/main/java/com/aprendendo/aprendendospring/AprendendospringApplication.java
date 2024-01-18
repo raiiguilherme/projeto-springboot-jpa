0d6596b4d69894291fbea38d71d60bdd2c41e97a
raiiguilherme <raiguilherme322@gmail.com>
2024-01-18 19:57:43 -0300

    criação do projeto

---
+++ src/main/java/com/aprendendo/aprendendospring/AprendendospringApplication.java
@@ -0,0 +1,14 @@
+package com.aprendendo.aprendendospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendendospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendendospringApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/aprendendo/aprendendospring/AprendendospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprendendo.aprendendospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprendendospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
