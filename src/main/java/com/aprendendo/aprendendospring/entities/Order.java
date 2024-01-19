2e1fd662b5da3e448f8cb5537944eb15714632ab
raiiguilherme <raiguilherme322@gmail.com>
2024-01-19 20:13:38 -0300

    Entidade Order e Associação

---
+++ src/main/java/com/aprendendo/aprendendospring/entities/Order.java
@@ -0,0 +1,87 @@
+package com.aprendendo.aprendendospring.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity		//identifica essa classe como sendo uma tabela do nosso banco de dados
+@Table (name="tb_order")	//adiciona o nome dessa tabela
+public class Order implements Serializable { //muito importante implementar o serializable
+
+	
+	private static final long serialVersionUID = 1L;
+	@Id				//identifica que esse atributo é um ID da tabela e define logo a baixo a estrategia de geração dele (Neste caso seria o AUTOINCREMENT)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private Instant moment;
+	
+	@ManyToOne		//define a relação das nossa entidades (no caso esta tendo uma relação muitos para um com a nossa entidade User)
+	@JoinColumn(name="client_id")	//define o nome dessa chave estrangeira
+	private User clients;
+	
+	public Order() {
+	
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public User getUsers() {
+		return clients;
+	}
+
+	public void setClients(User clients) {
+		this.clients = clients;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return id == other.id;
+	}
+
+	public Order(long id, Instant moment, User clients) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		this.clients = clients;
+	}
+	
+	
+	
+	
+	
+	
+}
--- src/main/java/com/aprendendo/aprendendospring/entities/User.java
+++ src/main/java/com/aprendendo/aprendendospring/entities/User.java
@@ -1,19 +1,29 @@
 package com.aprendendo.aprendendospring.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity			//dizendo que essa classe representa uma entidade do nosso banco 
 @Table(name="tb_user")	//dando um nome 
-public class User implements Serializable {
+public class User implements Serializable {//muito importante implementar o Serializable
 	
 	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "clients")	//define a relação "UM PARA MUITOS" na nossa relação com a entidade Order, 
+	private List<Order> orders = new ArrayList<>();	 //e direciona que na nossa entidade Order, o atributo que é a chave estrangeira é o "clients"
+	
+	
+	
+	
 	@Id	//anotação que identifica como ID
 	@GeneratedValue(strategy = GenerationType.IDENTITY) //gerando o autoincrement
 	private long id;
@@ -21,6 +31,7 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -84,6 +95,9 @@ public class User implements Serializable {
 	}
 	public User() {
 		
+	}
+	public List<Order> getOrders() {
+		return orders;
 	}
 	
 	
